aa5051ca843146396b9e94fa8a2e908f47c3c5cd
TimoWieme <1004149@hr.nl>
2022-07-25 11:41:50 +0200

    Finished main part of portfolio page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* ============ Typing animation ============ */
+
+var typed = new Typed(".typing", {
+    strings:["", "Website ontwikkelaar", "Website Designer", "Ondernemende technoloog", "Ontwerpende technoloog"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})  
