bf1df013b026c59c75f0572c068d8425037d853d
TimoWieme <1004149@hr.nl>
2022-07-26 15:00:52 +0200

    Added portfolio images

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* ============ Typing animation ============ */
 
 var typed = new Typed(".typing", {
-    strings:["", "Website ontwikkelaar", "Website Designer", "Ondernemende technoloog", "Ontwerpende technoloog"],
+    strings:["", "Student","Website ontwikkelaar", "Website Designer", "Ondernemende technoloog", "Ontwerpende technoloog"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
